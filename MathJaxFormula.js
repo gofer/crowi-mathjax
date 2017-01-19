f83dea45f725b7254e0cfe3069aa7af1ef443023
gofer <gofer@ex-studio.info>
2017-01-19 17:55:48 +0900

    modify MathJaxFormula.js to human-readable

--- MathJaxFormula.js
+++ MathJaxFormula.js
@@ -1,12 +1,42 @@
+/* MathJax Formula
+ *
+ * markdown: $$ y = f(x) $$
+ *   -> [ preprocess ] -> 
+ * markdown: <pre class="mathjax-formula">`$$ y = f(x) $$`</pre>
+ *   -> [ markdown parser (marked) ] -> 
+ * html: <pre class="mathjax-formula"><code>$$ y = f(x) $$</code></pre>
+ *   -> [ postprocess ] -> 
+ * html: $$ y = f(x) $$
+ */
+
 export default class MathJaxFormula {
+
   constructor() {
-    this.block_open_tag   = '<code class="mathjax-formula">`';
-    this.block_close_tag  = '`</code>';
-    this.inline_open_tag  = '<code class="mathjax-formula">`';
-    this.inline_close_tag = '`</code>';
+    this.block_open_tag   = '<div class="mathjax-formula-block">';
+    this.block_close_tag  = '</div>';
+    this.inline_open_tag  = '<span class="mathjax-formula-inline">';
+    this.inline_close_tag = '</span>';
+
+    this.get_tag_name = function(open_tag) {
+      return open_tag.replace(/^<(.*)>$/, '$1').split(' ')[0];
+    };
+
+    this.get_class_name = function(tag) {
+      return tag.replace(/^<(.*)>$/, '$1')
+             .split(' ')
+             .map( function(v) { return v.split('='); } )
+             .reduce(
+               function(p,v) { return (v[0] == 'class') ? v[1] : p; }, 
+               null
+             );
+    };
+
+    this.block_tag_name  = this.get_tag_name(this.block_open_tag);
+    this.inline_tag_name = this.get_tag_name(this.inline_open_tag);
+    this.block_class_name  = this.get_class_name(this.block_open_tag);
+    this.inline_class_name = this.get_class_name(this.inline_open_tag);
   }
 
-  // todo: ignore in `~~~` code span & ```~~~``` code block
   process(markdown) {
     String.prototype.insert = function(i, s) {
       return this.slice(0, i) + s + this.slice(i);
@@ -26,73 +56,98 @@ export default class MathJaxFormula {
       return pos + 1 < markdown.length && markdown[pos + 1] == '$';
     };
 
+    var set_code_flag = function(quote_num) { 
+      flag_in_code   = true;
+      code_quote_num = quote_num;
+    };
+
+    var unset_code_flag = function() {
+      flag_in_code   = false;
+      code_quote_num = 0;
+    };
+
+    var set_formula_flag = function(block) {
+      flag_in_formula    = true;
+      flag_block_formula = block;
+    };
+
+    var unset_formula_flag = function() {
+      flag_in_formula    = false;
+      flag_block_formula = false;
+    };
+
+    var count_code_quote = function() {
+      var p = pos;
+      while (p < markdown.length && markdown[p] == '`') {
+        p += 1;
+      }
+      return p - pos;
+    }
+
+    var has_expected_code_quote = function(expect) {
+      var p = pos;
+      for (var i = 0; p < markdown.length && i < expect; i += 1) {
+        if (markdown[p] != '`') {
+          break;
+        }
+        p += 1;
+      }
+      return (p - pos) == expect;
+    }
+
     var last_pos = -1;
  
     while (pos < markdown.length) {
       if (markdown[pos] == '`' && not_escaped()) {
-        if (flag_in_code) { // コード内
-          var p = pos;
-          for (var i = 0; p < markdown.length && i < code_quote_num; i += 1) {
-            if(markdown[p] != '`') break;
-            p += 1;
-          }
-          if ((p - pos) == code_quote_num) {
-            flag_in_code = false;
-            code_quote_num = 0;
-          }
-          pos = p;
-        } else {
-          flag_in_code = true;
-          var p = pos;
-          while (p < markdown.length && markdown[p] == '`') {
-            p += 1;
-          }
-          code_quote_num = p - pos;
-          pos = p;
-        }
-      } else if (markdown[pos] == '$' && not_escaped()) {
-        if (flag_in_code) { // コード内
-          // 何もしない
-        } else if (flag_in_formula) { // 数式モード内
-          if (flag_block_formula) {
-            if (double_dollar()) {
-              markdown = markdown.insert(pos + 2, this.block_close_tag);
-              pos += this.block_close_tag.length + 1;
- 
-              flag_in_formula = false;
-              flag_block_formula = false;
-            }
-          } else {
-            markdown = markdown.insert(pos + 1, this.inline_close_tag);
-            pos += this.inline_close_tag.length;
+        if (flag_in_code && has_expected_code_quote(code_quote_num)) { // コード内
+            pos += code_quote_num;
 
-            flag_in_formula = false;
-            flag_block_formula = false;
+            unset_code_flag();
+        } else {
+          var num_of_quote = count_code_quote();
+          pos += num_of_quote;
+
+          set_code_flag(num_of_quote);
+        }
+      } else if (markdown[pos] == '$' && not_escaped() && (!flag_in_code)) {
+        if (flag_in_formula) { // 数式モード内
+          if (flag_block_formula && double_dollar()) {
+            markdown = markdown.insert(pos + 2, '`' + this.block_close_tag);
+            pos += this.block_close_tag.length + 2;
+
+            unset_formula_flag(); 
+          } else if ((!flag_block_formula) && (!double_dollar())) {
+            markdown = markdown.insert(pos + 1, '`' + this.inline_close_tag);
+            pos += this.inline_close_tag.length + 1;
+
+            unset_formula_flag();
           }
         } else {
-          flag_in_formula = true;
-          last_pos = pos;
-          
           if (double_dollar()) {
-            markdown = markdown.insert(pos, this.block_open_tag);
-            pos += this.block_open_tag.length + 1;
-            flag_block_formula = true;
+            markdown = markdown.insert(pos, this.block_open_tag + '`');
+            pos += this.block_open_tag.length + 2;
+
+            set_formula_flag(true);
           } else {
-            markdown = markdown.insert(pos, this.inline_open_tag);
-            pos += this.inline_open_tag.length;
-            flag_block_formula = false;
+            markdown = markdown.insert(pos, this.inline_open_tag + '`');
+            pos += this.inline_open_tag.length + 1;
+
+            set_formula_flag(false);
           }
+
+          last_pos = pos;
         }
       }
 
       pos += 1;
     }
 
+    // for invalid tex formula
     if (flag_in_formula) {
       if (flag_block_formula) {
-        markdown = markdown.insert(last_pos + this.block_open_tag.length  + 2, '$$' + this.blockclose_tag);
+        markdown = markdown.insert(last_pos + this.block_open_tag.length  + 2, '$$`' + this.block_close_tag);
       } else {
-        markdown = markdown.insert(last_pos + this.inline_open_tag.length + 1, '$'  + this.inline_close_tag);
+        markdown = markdown.insert(last_pos + this.inline_open_tag.length + 1, '$`'  + this.inline_close_tag);
       }
     }
 
@@ -109,59 +164,54 @@ export default class MathJaxFormula {
     var stack = [];
     var pos = 0;
 
+    var get_tag = function() {
+      var p = 0;
+      for (p = pos + 1; p < html.length; p += 1) {
+        if (html[p] == '>') {
+          break;
+        }
+      }
+      return html.slice(pos, p + 1);
+    };
+
     while (pos < html.length) {
       if (html[pos] == '<') {
-        var start_pos = pos;
-        var tag = '';
+        var tag = get_tag();
+        var tag_name = this.get_tag_name(tag);
         
-        var end_pos = pos + 1;
-        while (end_pos < html.length && html[end_pos] != '>') {
-          tag += html[end_pos];
-          end_pos += 1;
-        }
-
-        var tag_name = tag.split(' ')[0];
-        
-        if (tag_name.startsWith('/')) {
+        if (tag_name.startsWith('/')) { // 閉じタグ
           var open_tag = stack[stack.length - 1];
-          if ('/' + open_tag.TagName == tag_name) {
-            if (open_tag.ClassName == '"mathjax-formula"') {
-              html = html.remove(start_pos - '</code>'.length, end_pos);
-              if (open_tag.TagName == 'pre') {
-                end_pos -= this.block_close_tag.length + '</code>'.length;
-              } else {
-                end_pos -= this.inline_close_tag.length + '</code>'.length;
-              }
+          if ('/' + open_tag.TagName == tag_name) { // 正しい閉じタグ
+            if (
+              (open_tag.ClassName == this.block_class_name)
+              ||
+              (open_tag.ClassName == this.inline_class_name)
+            )  {
+              var end_pos = pos + tag.length - 1;
+              html = html.remove(pos - '</code>'.length, end_pos);
             }
 
             stack.pop();
           }
-        } else {
-          var class_name = '';
-          tag.split(' ').forEach(function(s){
-            var a = s.split('=');
-            if (a[0].startsWith('class')) {
-              class_name = a[1];
-            }
-          });
+        } else { // 開きタグ
+          var class_name = this.get_class_name(tag);
 
           stack.push({
             'TagName': tag_name, 
-            'ClassName': class_name, 
-            'StartPosition': start_pos,
-            'EndPosition': end_pos
+            'ClassName': class_name 
           });
         }
 
-        pos = end_pos;
+        pos += tag.length - 1;
       }
 
       pos += 1;
     }
 
-    html = html.replace(/<pre class=\"mathjax-formula\"><code>/g, '');
-    html = html.replace(/<code class=\"mathjax-formula\"><code>/g, '');
+    html = html.replace(new RegExp(this.block_open_tag  + '<code>', 'g'), '');
+    html = html.replace(new RegExp(this.inline_open_tag + '<code>', 'g'), '');
 
     return html;
   }
+
 }
